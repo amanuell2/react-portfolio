7447bdf4966fd93da5276d5d01d69146e141981f
amanuel <amanuel0000@gmail.com>
2021-02-13 15:01:04 -0800

    about us section styled

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react'
 import AboutUs from './pages/AboutUs';
 import Services from './pages/Services';
 import FaqSection from './component/FaqSection';
+import GlobalStyle from './component/GlobalStyle'
 function App() {
   return (
     <>
+      <GlobalStyle />
       <AboutUs />
       <Services />
       <FaqSection />
