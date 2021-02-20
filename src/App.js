0ed6f6cd3aa56d1f0be5489280385bb6c62432d9
amanuel <amanuel0000@gmail.com>
2021-02-21 00:44:07 +0300

    nav bar added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import AboutUs from './pages/AboutUs';
 import Services from './pages/Services';
 import FaqSection from './component/FaqSection';
+import Nav from "./component/Nav";
 import GlobalStyle from './component/GlobalStyle'
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Nav />
       <AboutUs />
       <Services />
       <FaqSection />
